30e63def6c60bbbdb9d1c5dfdbec2ec7b825983c
Saeed Rahimi <saeedr.com@gmail.com>
2019-10-05 21:06:23 +0330

    initial commit

---
+++ DotnetCoreGitlabHook/DotnetCoreGitlabHook/Controllers/WebhookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotnetCoreGitlabHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebhookController : ControllerBase
+    {
+ 
+
+        private readonly ILogger<WebhookController> _logger;
+
+        public WebhookController(ILogger<WebhookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "test";
+        }
+    }
+}
